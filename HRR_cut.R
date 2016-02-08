e75109862e14e008de44bdfed365c7758c4bbb1e
EA2106-Universite-Francois-Rabelais <thomas.duge@univ-tours.fr>
2016-02-08 10:44:48 +0100

    Second commit

---
+++ HRR_cut.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+library(optparse)
+library(parallel)
+
+option_list = list(make_option(c("-n", "--files"), type="integer", default=NULL, help="Number of .txt files (equal to number of cpus used in hrr)", metavar="integer"),
+		   make_option(c("-a", "--accession"), type="character", default=NULL, help="File containing transcript names", metavar="character"),
+		   make_option(c("-s", "--size"), type="integer", default=NULL, help="Number of rows in the processed matrix (not REAL size), see hrr standard output", metavar="integer"),
+		   make_option(c("-S", "--realsize"), type="integer", default=NULL, help="Number of rows in the matrix (REAL size), see hrr standard output", metavar="integer"),
+		   make_option(c("-c", "--subsize"), type="integer", default=NULL, help="Number of row in the submatrix, see hrr standard output", metavar="integer"),
+		   make_option(c("-r", "--corr"), type="character", default=NULL, help="PCC or HRR", metavar="character"),
+		   make_option(c("-m", "--mcores"), type="integer", default=NULL, help="Number of CPUs to use; relies on 'Parallel' R package", metavar="integer"))
+		   
+ 
+opt_parser<-OptionParser(option_list=option_list)
+opt<-parse_args(opt_parser)
+
+if (is.null(opt$files)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+
+cpus.ok<-opt$files-1
+acc.files<-paste(0:cpus.ok, ".txt", sep="")
+ids<-scan(opt$accession, what="character")
+to.add<-opt$size-opt$realsize
+annot<-c(acc.files, rep(acc.files[length(acc.files)], to.add))
+paquets.names<-seq(1,nrow(annot), opt$subsize)
+paquets.names<-c(paquets.names, length(annot)+1)
+
+
+mclapply(1:length(acc.files), function(y){
+        a<-scan(acc.files[y], what="character")
+        acc.names<-annot[paquets.names[y]:(paquets.names[y+1]-1),2]
+        paquets<-seq(1,length(a), opt$realsize)
+        paquets<-c(paquets, length(a)+1)
+        lapply(1:(length(paquets)-1), function(x){
+                  file.name.tmp<-paste(opt$corr, "_lt/", as.vector(acc.names[x]), sep="")
+                  table.tmp<-cbind.data.frame("lt"=annot[,2], "corr"=as.numeric(a[paquets[x]:(paquets[(x+1)]-1)]))
+                  write.table(table.tmp, file.name.tmp)})
+        }, mc.cores=opt$mcores)
+~                                   
